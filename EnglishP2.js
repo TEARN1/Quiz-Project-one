73d892b9205f9ce6f9a14f0a0c179487ecc28899
Asemahle Nkwali <99318798+TEARN1@users.noreply.github.com>
2023-12-19 13:11:12 +0200

    Add files via upload
    First paper two English paper

---
+++ EnglishP2.js
@@ -0,0 +1,245 @@
+const EnglishFirstAdditionalLanguage2023DecemberExam = [{
+    question : "Arthur Jarvis career !",
+    answer : [
+        {text: "a politician", correct : false},
+        {text: "an engineer", correct : true},
+        {text: "Ga burglar", correct : false},
+        {text: "a priest", correct : false}
+    ]
+},{
+    question : "Matthew Kumalo career?",
+    answer : [
+        {text: "a burglar", correct : true},
+        {text: "an engineer", correct : false},
+        {text: "an agriculturalist", correct : false},
+        {text: "a politician", correct : false}
+    ]
+},{
+    question : "John Kumalo Career ?",
+    answer : [
+        {text: "a burglar", correct : false},
+        {text: "an agriculturalist", correct : false},
+        {text: "a politician", correct : true},
+        {text: "an engineer", correct : false}
+    ]
+},{
+    question : "Napolean Letsitsi Career ?",
+    answer : [
+        {text: "a politician", correct : false},
+        {text: "an agriculturalist", correct : true},
+        {text: "an engineer", correct : false},
+        {text: "a burglar", correct : false}
+    ]
+},{
+    question : "What does this extract reveal about Gertrude's character",
+    answer : [
+        {text: "She is a go getter", correct : false},
+        {text: " She is a prostitute", correct : true},
+        {text: "Gertrude is immoral", correct : true},
+        {text: "She handles things perfectly", correct : false}
+    ]
+},{
+    question : "Describe how Msimangu displays his caring nature towards Stephen while he is in Johannesburg",
+    answer : [
+        {text: "Msimangu finds a room for Stephen at Mrs Lithebe's house", correct : true},
+        {text: "He assists Stephen in his quest to find Absalom", correct : true},
+        {text: "He prays for him and with him", correct : true},
+        {text: "He accompanies him when he goes in search of Absalom's pregnant girlfriend", correct : true}
+    ]
+},{
+    question : "Refer to line 12 ('I do not … no husband now'), What tone would Msimangu use in these lines?",
+    answer : [
+        {text: "sarcastic", correct : true},
+        {text: "concerned", correct : true},
+        {text: "serious", correct : true},
+        {text: "grave", correct : true}
+    ]
+},{
+    question : "Refer to line 12 ('I do not … no husband now'), Why would Msimangu use this tone in these lines ?",
+    answer : [
+        {text: "Just get him off the way", correct : false},
+        {text: "immoral way of living./He is concerned about Gertrude's immoral way of living", correct : true},
+        {text: "To make him look away", correct : false},
+        {text: "All he cares about is himself", correct : false}
+    ]
+},{
+    question : "Explain the irony in Gertrude's reason for going to Johannesburg",
+    answer : [
+        {text: "She found what she was looking for", correct : false},
+        {text: "In Gertrude's search for her husband, she instead finds 'many husbands'", correct : true},
+        {text: "She found the future she was dreaming about", correct : false},
+        {text: "She found nothing", correct : false}
+    ]
+},{
+    question : "One of the themes in Cry, the Beloved Country is pain and suffering",
+    answer : [
+        {text: "James Jarvis suffers much pain and heartache when his son is killed", correct : true},
+        {text: "Margaret Jarvis dies from a broken heart because of Arthur's death", correct : true},
+        {text: "When Absalom is hanged, Stephen and Mrs Kumalo endure tremendous pain and suffering", correct : true},
+        {text: "Pain and suffering make us grow strong", correct : false}
+    ]
+},{
+    question : "Refer to the novel as a whole. Do you have sympathy for Stephen Kumalo?",
+    answer : [
+        {text: "Yes, Stephen Kumalo encounters challenges on his journey to Johannesburg where he is tricked out of his money.", correct : true},
+        {text: "Yes, He is pained/tortured by the decisions made by some of his family members and the consequences they face as a result of their bad decisions", correct : true},
+        {text: "No, Stephen Kumalo must not allow his brother's way of living to affect him", correct : true},
+        {text: "No, He is not responsible for Gertrude's choice to continue living an immoral life", correct : true}
+    ]
+},{
+    question : ["EXTRACT B","The Kumalos receive the news"," So he read no more but sat there an hour, two hours maybe. Indeed, he neither saw sight nor heard sound till his wife said to him, It has come then, Stephen. And when he nodded, she said, Give it to me, Stephen. With shaking hands he gave it to her, and she read it also, and sat looking before her, with hands he gave it to her, and she read it also, and sat looking before her, with lost and terrible eyes, for this was the child of her womb, of her breasts. Yet she did not sit as long as he had done, for she stood up and said, It is not good to sit idle. Finish your letters, and go to see Kuluse's child, and the girl Elizabeth that is ill. And I shall do my work about the house. – There is another letter, he said. – From him? she said. – From him. He gave it to her, and she sat down again and opened it carefully and read it. The pain was in her eyes and her face and her hands, but he did not see it, for he stared before him on the floor, only his eyes were not looking at the floor but at no place at all, and his face was sunken, in the same mould of suffering from which it had escaped since his return to this valley. – Stephen, she said sharply....................... Describe the setting of this extract."],
+    answer : [
+        {text: "The setting is at the Kumalo home after the delivery of letters (by the child).", correct : true},
+        {text: "At kumalo's in the morning after drinking tea ", correct : false},
+        {text: "After party, on the way home", correct : false},
+        {text: "While waiting for delivery latter from the child", correct : false}
+    ]
+},{
+    question : "With shaking hands … of her breasts...... -What do these lines tell us about Mrs Kumalo's state of mind?",
+    answer : [
+        {text: "She is anguished/agonised/distressed/in shock/pain", correct : true},
+        {text: "Mrs Kumalo has just received the news that Absalom Kumalo (her baby/the child to whom she gave birth) will be hanged.", correct : true},
+        {text: "happy full of joy about what is going on", correct : false},
+        {text: "Mrs Kumalo has just recieved the news that Absalom Kumalo is arrested", correct : false}
+    ]
+},{
+    question : "Arthur Jarvis is ...",
+    answer : [
+        {text: "drowned by Johannes.", correct : false},
+        {text: "shot by Absalom.", correct : true},
+        {text: "stabbed by Matthew.", correct : false},
+        {text: "choked by John.", correct : false}
+    ]
+},{
+    question : "Why does Mrs Kumalo want Stephen Kumalo to see Kuluse's child at this point in the novel ? ",
+    answer : [
+        {text: "Mrs Kumalo wants to distract Stephen Kumalo.", correct : true},
+        {text: "As a priest, it is his duty to administer to Kuluse's ill/dying child.", correct : true},
+        {text: "To do something bad and claim the child was sick", correct : false},
+        {text: "To form a very good bond between them", correct : false}
+    ]
+},{
+    question : "and his face … mould of suffering ... Identify the figure of speech in these lines.",
+    answer : [
+        {text: "Proverb", correct : false},
+        {text: "Verb", correct : false},
+        {text: "Adjective", correct : false},
+        {text: "Metaphor", correct : true}
+    ]
+},{
+    question : "and his face … mould of suffering ...Explain why the figure of speech is relevant in this extract",
+    answer : [
+        {text: "Kumalo's face is distorted with pain/anguish/sadness", correct : true},
+        {text: "which highlights/shows the pain he is experiencing after receiving the news/knowledge that his son will be hanged", correct : true},
+        {text: "Because he lost a lot of money", correct : false},
+        {text: "Because he was sick and he could not hide it anymore", correct : false}
+    ]
+},{
+    question : "Why is the following statement FALSE? ........... In his final letter, Msimangu includes money as a gift to Stephen Kumalo",
+    answer : [
+        {text: "Msimangu hands the money to him (at a farewell function) in Johannesburg.", correct : true},
+        {text: "He was given the money in Cape Town", correct : false},
+        {text: "It was not money, but a gold ring", correct : false},
+        {text: "He never wrote a letter and there was no money", correct : false}
+    ]
+},{
+    question : "What happens to Margaret Jarvis?",
+    answer : [
+        {text: "She Finds love that lasts forever", correct : false},
+        {text: "She travels out of the country", correct : true},
+        {text: "She becomes rich", correct : false},
+        {text: "Margaret Jarvis passes away/dies", correct : true}
+    ]
+},{
+    question : "The novel ends on a hopeful note. The novel ends on a hopeful note.",
+    answer : [
+        {text: "Yes, Despite his sadness, Kumalo does not lose hope. His deep faith in God carries him as he prays and meditates on the mountain.", correct : true},
+        {text: "Yes, There is hope for the people of Ndotsheni as their living conditions improve with the support given by James Jarvis. ", correct : true},
+        {text: "No, The novel ends with the hanging of Absalom and all hope for his life is dashed. ", correct : true},
+        {text: "No, James Jarvis intends leaving High Place which will sever the bond between him and Stephen Kumalo ", correct : true}
+    ]
+}
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = EnglishFirstAdditionalLanguage2023DecemberExam[currentQuestionIndex];
+    let questionNo = currentQuestionIndex +1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answer.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer)
+    })
+}
+
+function resetState(){
+    nextButton.style.display = "none"
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e) {  
+    const selectBtn = e.target;
+    const isCorrect = selectBtn.dataset.correct === "true";
+    if(isCorrect) {
+        selectBtn.classList.add("correct");
+        score++
+    }else {
+        selectBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disable = "true"
+    });
+    nextButton.style.display = "block"
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${EnglishFirstAdditionalLanguage2023DecemberExam.length}!`
+    nextButton.innerHTML = "Play again";
+    nextButton.style.display ="block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++
+    if(currentQuestionIndex < EnglishFirstAdditionalLanguage2023DecemberExam.length){
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if(currentQuestionIndex < EnglishFirstAdditionalLanguage2023DecemberExam.length){
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
+
+startQuiz();
